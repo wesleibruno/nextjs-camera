71cdd76b96d9409073ac6ecc3761d7963664754b
Weslei Bruno <weslei945@gmail.com>
2024-05-25 17:26:58 -0300

    switch camera

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   const webcamRef = useRef(null);
   const [isCameraOn, setIsCameraOn] = useState(false);
   const [isAudioOn, setIsAudioOn] = useState(true);
+  const [facingMode, setFacingMode] = useState('user'); // 'user' para câmera frontal, 'environment' para câmera traseira
 
   const toggleCamera = () => {
     setIsCameraOn((prevState) => !prevState);
@@ -20,14 +21,27 @@ export default function Home() {
     setIsAudioOn(true);
   };
 
+  const switchCameraFacingMode = () => {
+    setFacingMode((prevFacingMode) =>
+      prevFacingMode === 'user' ? 'environment' : 'user'
+    );
+  };
+
   return (
     <div>
       <h1>Webcam Example</h1>
       {isCameraOn ? (
         <div>
-          <Webcam audio={isAudioOn} ref={webcamRef} />
+          <Webcam
+            audio={isAudioOn}
+            videoConstraints={{ facingMode: facingMode }}
+            ref={webcamRef}
+          />
           <button onClick={toggleCamera}>Stop Camera</button>
-          <button onClick={toggleAudio}>{isAudioOn ? 'Pause Audio' : 'Resume Audio'}</button>
+          <button onClick={toggleAudio}>
+            {isAudioOn ? 'Pause Audio' : 'Resume Audio'}
+          </button>
+          <button onClick={switchCameraFacingMode}>Switch Camera</button>
         </div>
       ) : (
         <div>
